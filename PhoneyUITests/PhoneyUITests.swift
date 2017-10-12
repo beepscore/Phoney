7ce9a4a4e93d836d21a7e07fd24a99cf39a3b695
beepscore <support@beepscore.com>
2017-10-11 23:52:44 -0700

    Test is tapping call and alert, but not End call button

--- PhoneyUITests/PhoneyUITests.swift
+++ PhoneyUITests/PhoneyUITests.swift
@@ -9,7 +9,11 @@
 import XCTest
 
 class PhoneyUITests: XCTestCase {
-        
+
+    let app = XCUIApplication()
+    // https://stackoverflow.com/questions/9910366/what-is-the-bundle-identifier-of-apples-default-applications-in-ios
+    let phoneApp = XCUIApplication(bundleIdentifier: "com.apple.mobilephone")
+
     override func setUp() {
         super.setUp()
         
@@ -21,6 +25,7 @@ class PhoneyUITests: XCTestCase {
         XCUIApplication().launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+
     }
     
     override func tearDown() {
@@ -32,22 +37,38 @@ class PhoneyUITests: XCTestCase {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
 
-        // handle alert shown by iOS
+        // handle system alert shown by iOS
+        // http://masilotti.com/ui-testing-cheat-sheet/
         addUIInterruptionMonitor(withDescription: "555-1212") { (alert) -> Bool in
             alert.buttons["Call"].tap()
+            print("*** tapped alert Call")
             return true
         }
 
-        let app = XCUIApplication()
-        app.buttons["call 555-1212"].tap()
+        let appCallButton = app.buttons["call 555-1212"]
+        if appCallButton.waitForExistence(timeout: 5) {
+            appCallButton.tap()
+            print("*** tapped app call 555-1212")
 
-        // recorded this, but when running in test it threw error
-        // https://stackoverflow.com/questions/40403805/dismissing-alert-xcuitest
-        // app.alerts["‭555-1212‬"].buttons["Call"].tap()
+            // recorded this, but when running in test it threw error
+            // https://stackoverflow.com/questions/40403805/dismissing-alert-xcuitest
+            // app.alerts["‭555-1212‬"].buttons["Call"].tap()
 
-        // interact with app to cause handler to fire
-        // https://stackoverflow.com/questions/32148965/xcode-7-ui-testing-how-to-dismiss-a-series-of-system-alerts-in-code?rq=1
-        app.tap()
+            // interact with app to cause system alert handler to fire
+            // https://stackoverflow.com/questions/32148965/xcode-7-ui-testing-how-to-dismiss-a-series-of-system-alerts-in-code?rq=1
+            app.tap()
+
+
+            let endCallButton = phoneApp.buttons["End call"]
+            // https://dzone.com/articles/new-xcuitest-features-with-xcode-9-hands-on-explor
+            if endCallButton.waitForExistence(timeout: 20) {
+                endCallButton.tap()
+                print("*** tapped endCallButton")
+            }
+
+        }
+
+
 
         // https://stackoverflow.com/questions/28821722/delaying-function-in-swift
 //        let nowPlusDelay = DispatchTime.now() + .seconds(20)
