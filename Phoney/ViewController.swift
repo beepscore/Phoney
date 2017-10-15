ee1e66692a16454ae06fe63c51ea856bbc1d84f8
beepscore <support@beepscore.com>
2017-10-15 15:40:05 -0700

    In ViewController simplify callTapped, deleted print statement, deleted call to canOpenURL

--- Phoney/ViewController.swift
+++ Phoney/ViewController.swift
@@ -31,15 +31,9 @@ class ViewController: UIViewController {
     /// on device, when user taps button, iOS shows an alert with "Cancel" and "Call"
     /// tapping call dismisses alert, navigates to phone app, makes call
     @IBAction func callTapped(_ sender: Any) {
-        print("callTapped")
-
         // https://stackoverflow.com/questions/27259824/calling-a-phone-number-in-swift
         if let phoneCallURL = URL(string: "tel://\(wellsFargoPhoneNumber)") {
-
-            let application:UIApplication = UIApplication.shared
-            if (application.canOpenURL(phoneCallURL)) {
-                application.open(phoneCallURL, options: [:], completionHandler: nil)
-            }
+            UIApplication.shared.open(phoneCallURL, options: [:], completionHandler: nil)
         }
     }
     
