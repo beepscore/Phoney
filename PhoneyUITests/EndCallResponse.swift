90687c10635a55ad375740bb24b8f3d213743549
beepscore <support@beepscore.com>
2017-10-21 16:25:56 -0700

    In EndCallResponse update to match service_piface.py for piface relay

--- PhoneyUITests/EndCallResponse.swift
+++ PhoneyUITests/EndCallResponse.swift
@@ -9,14 +9,22 @@
 import Foundation
 
 // https://developer.apple.com/documentation/swift/codable
-// example
+// example response from service.py for gpio
 // EndCallResponseData(error: nil, new_value: 0,
 // pin_direction: "output", pin_name: "OUT_24", pin_number: "24", status: "SUCCESS")
+// struct EndCallResponse: Codable {
+//     var error: String?
+//     var new_value: Int
+//     var pin_direction: String
+//     var pin_name: String
+//     var pin_number: String
+//     var status: String
+// }
+
+// example response data from service_piface.py for piface relay
 struct EndCallResponse: Codable {
-    var error: String?
-    var new_value: Int
-    var pin_direction: String
-    var pin_name: String
-    var pin_number: String
+    var api_name: String
+    var response: String
     var status: String
+    var version: String
 }
